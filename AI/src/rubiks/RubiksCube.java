264f35c9cf052e8b34a8dd336d9c02d4a98c4a5c
Braemen Stoltz <braemen@shaw.ca>
2016-01-13 23:27:34 -0800

    switch from string to character array implementation

--- AI/src/rubiks/RubiksCube.java
+++ AI/src/rubiks/RubiksCube.java
@@ -5,43 +5,57 @@ package rubiks;
 //0..S-1 slices per axis of rotation
 //which means numMovesPossible = #axis * #directions * size;
 
+
+/*
+ * one Face for size==2 cube
+ * ______________________
+ * | [0, 0]  |  [0, 1]  |
+ * |_________|__________|
+ * | [1, 0]  |  [1, 1]  |
+ * |_________|__________|
+ * 
+ * have 6 of these representing the faces
+ * the coordinates represent the cubies
+ * 
+ */
 public class RubiksCube implements Searchable
 {
 	private int size; //size=2 when dealing 2x2x2 cube & vice versa
-	private String cube;
-	private int numMovesPossible;
+	//representations of cube
+	private char[] cubeStr;
+	private int numMovesPossible; //cap on the amount of moves possible
 	
 	public RubiksCube(int size) {
 		this.size = size;
-		cube = createSolvedCube();
+		cubeStr = createSolvedCube();
 		numMovesPossible = 3*2*size;
 	}
 
 	//GGGGRRRRWWWWYYYYBBBBOOOO
-	private String createSolvedCube() {
-		String cubeStr = "";
+	private char[] createSolvedCube() {
+		char[] cubeStr = new char[size*size*6];
 		for(int i=0; i<6; i++) {
-			String color = "";
+			char color = ' ';
 			switch(i) {
-			case 0: color = "G"; break;
-			case 1: color = "R"; break;
-			case 2: color = "W"; break;
-			case 3: color = "Y"; break;
-			case 4: color = "B"; break;
-			case 5: color = "O"; break;
+			case 0: color = 'G'; break;
+			case 1: color = 'R'; break;
+			case 2: color = 'W'; break;
+			case 3: color = 'Y'; break;
+			case 4: color = 'B'; break;
+			case 5: color = 'O'; break;
 			}
 			for(int j=0; j<size*size; j++) {
-				cubeStr += color;
+				cubeStr[j] = color;
 			}
 		}
 		return cubeStr;
 	}
 	
 	public boolean isSolved() {
-		String cubeCopy = cube;
-		String testStr = "";
+		char[] cubeCopy = cubeStr;
+		char[] testStr = new char[size*size*6];
 		int i = 0;
-		while(!cubeCopy.isEmpty()) {
+		while() { //while(cube isnt empty)
 			System.out.println(cubeCopy);
 			testStr = cubeCopy.substring(0, size*size);
 			System.out.println(testStr);
@@ -54,7 +68,7 @@ public class RubiksCube implements Searchable
 		return true;
 	}
 	
-	public String getCube() {
-		return cube;
+	public char[] getCube() {
+		return cubeStr;
 	}
 }

6cfd4197cfa7dae1aa641744b62d4299786c5464
Braemen Stoltz <braemen@shaw.ca>
2016-01-16 17:26:43 -0800

    i hate this project

--- AI/src/rubiks/Move.java
+++ AI/src/rubiks/Move.java
@@ -1,6 +1,7 @@
 package rubiks;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Move
 {	
@@ -16,7 +17,7 @@ public class Move
 		int from_face, to_face, from_row = -40, to_row, from_col = -40, to_col;
 		char[][][] cubeArr = cube.getCube();
 		char value_bfr[] = new char[size];
-		int[] effectedFaces = findEffectedFaces(axis);
+		int[] effectedFaces = findEffectedFaces(axis, dir);
 		
 		/* Dealing with edge cases */
 		boolean isEdgeCase;
@@ -33,17 +34,11 @@ public class Move
 		for(int i=0, j=effectedFaces.length; i<effectedFaces.length && j>0 ;i++, j--) {
 			/*set from_face and to_face based on direction*/
 			from_face = effectedFaces[i];
-			if(dir.equals(Direction.CW))
-				if(i == effectedFaces.length-1)
-					to_face = effectedFaces[0];
-				else
-					to_face = effectedFaces[i+1];
-			else {
-				if(j == 0)
-					to_face = effectedFaces[effectedFaces.length-1];
-				else
-					to_face = effectedFaces[j-1];
-			}
+			if(i == effectedFaces.length-1)
+				to_face = effectedFaces[0];
+			else
+				to_face = effectedFaces[i+1];
+			System.out.println(from_face+" "+to_face+" "+axis);
 			
 			/* Varies based on axis section */
 			//store the values of the face in the value buffer if this
@@ -63,14 +58,14 @@ public class Move
 					}
 			}
 					
+			//get the faces values then move?
+			
+			
 			//do the actual moving of rows/columns, juggling of characters, etc.
 			for(int k=0, l=size; k<size && l>0; k++, l--) {
 				switch(axis) {
 				case X:
-					if(from_face==4 || from_face==2)
-						from_row = size-1-k;
-					else
-						from_row = k;
+					from_row = k;
 					from_col = sliceNum;
 					to_col = from_col;
 					//[even to even] OR [odd to odd]
@@ -83,10 +78,7 @@ public class Move
 					break;
 				case Y: 
 					from_row = size-1-sliceNum;
-					if(from_face==0 || from_face==4)
-						from_col = size-1-k;
-					else
-						from_col = k;
+					from_col = k;
 					to_row = from_row;
 					if((from_face%2==0 && to_face%2==0) || (from_face%2==1 && to_face%2==1)) {
 						to_col = from_col;
@@ -100,15 +92,14 @@ public class Move
 						from_row = sliceNum;
 						from_col = k;
 					}
+					//from 1 to 2 switch columns
+					//3 to 0 switch rows
 					else { //1,0
 						from_row = k;
 						from_col = size-1-sliceNum;
 					}
 					if((from_face%2==0 && to_face%2==0) || (from_face%2==1 && to_face%2==1)) {
-						if(from_face == 3)
-							to_row = size-1-from_col;
-						else
-							to_row = from_col;
+						to_row = from_col;
 						to_col = size-1-from_row;
 					}
 					else {
@@ -123,9 +114,14 @@ public class Move
 					to_col = -1;
 					break;
 				}
+
+				System.out.println(from_row+" "+from_col+" FROM "+axis+".. BUFFER: "+Arrays.toString(value_bfr));
+				System.out.println(to_row+" "+to_col+" TO "+axis);
+				
 				char tmp = cubeArr[to_face][to_row][to_col]; //new to_face value
 				cubeArr[to_face][to_row][to_col] = value_bfr[k]; //fill to_face value with from_face value
 				value_bfr[k] = tmp; //write new value to buffer
+				
 			}//for moving of rows/columns
 			
 		}//for all effected faces
@@ -181,17 +177,35 @@ public class Move
 		}
 	}
 	//faces effected on move
-	private static int[] findEffectedFaces(Axis axis) {
+	private static int[] findEffectedFaces(Axis axis, Direction dir) {
 		switch(axis) {
 		case X:
-			int[] effectedX = { 5, 3, 4, 2 };
-			return effectedX;
+			if(dir.equals(Direction.CW)) {
+				int[] effectedX = { 5, 3, 4, 2 };
+				return effectedX;
+			}
+			else {
+				int[] effectedX = { 5, 2, 4, 3 };
+				return effectedX;
+			}
 		case Y:
-			int[] effectedY = { 5, 0, 4, 1 };
-			return effectedY;
+			if(dir.equals(Direction.CW)) {
+				int[] effectedY = { 5, 0, 4, 1 };
+				return effectedY;
+			}
+			else {
+				int[] effectedY = { 5, 1, 4, 0 };
+				return effectedY;
+			}
 		case Z:
-			int[] effectedZ = { 2, 0, 3, 1 };
-			return effectedZ;
+			if(dir.equals(Direction.CW)) {
+				int[] effectedZ = { 3, 1, 2, 0 };
+				return effectedZ;
+			}
+			else {
+				int[] effectedZ = { 3, 0, 2, 1 };
+				return effectedZ;
+			}
 		default: return null;
 		}
 	}
--- AI/src/rubiks/MoveTest.java
+++ AI/src/rubiks/MoveTest.java
@@ -13,8 +13,6 @@ public class MoveTest {
 		RubiksCube cube = new RubiksCube(3);
 		//do and undo a move
 		Move.move(cube, cube.getSize(), 0, Axis.X, Direction.CW);
-		Move.move(cube, cube.getSize(), 0, Axis.X, Direction.CW);
-		Move.move(cube, cube.getSize(), 0, Axis.X, Direction.CCW);
 		Move.move(cube, cube.getSize(), 0, Axis.X, Direction.CCW);
 		assertTrue(cube.isSolved());
 	}
@@ -36,8 +34,8 @@ public class MoveTest {
 		RubiksCube cube = new RubiksCube(3);
 		//do and undo a move
 									//shit
-		Move.move(cube, cube.getSize(), 2, Axis.Z, Direction.CCW);
-		Move.move(cube, cube.getSize(), 2, Axis.Z, Direction.CW);
+		Move.move(cube, cube.getSize(), 0, Axis.Z, Direction.CCW);
+		Move.move(cube, cube.getSize(), 0, Axis.Z, Direction.CW);
 		assertTrue(cube.isSolved());
 	}
 
@@ -45,10 +43,15 @@ public class MoveTest {
 	public void moveXYAxisTest() {
 		RubiksCube cube = new RubiksCube(3);
 		//do and undo a move
+		System.out.println(Arrays.deepToString(cube.getCube()));
 		Move.move(cube, cube.getSize(), 0, Axis.Y, Direction.CCW);
+		System.out.println(Arrays.deepToString(cube.getCube()));
 		Move.move(cube, cube.getSize(), 0, Axis.X, Direction.CW);
+		System.out.println(Arrays.deepToString(cube.getCube()));
 		Move.move(cube, cube.getSize(), 0, Axis.X, Direction.CCW);
+		System.out.println(Arrays.deepToString(cube.getCube()));
 		Move.move(cube, cube.getSize(), 0, Axis.Y, Direction.CW);
+		System.out.println(Arrays.deepToString(cube.getCube()));
 		assertTrue(cube.isSolved());
 	}
 	
@@ -76,21 +79,14 @@ public class MoveTest {
 	
 	@Test
 	public void moveXYZAxisTest() {
-		RubiksCube cube = new RubiksCube(3);
+		RubiksCube cube = new RubiksCube(2);
 		//do and undo a move
-		System.out.println(Arrays.deepToString(cube.getCube()));
 		Move.move(cube, cube.getSize(), 0, Axis.X, Direction.CW);
-		System.out.println(Arrays.deepToString(cube.getCube()));
 		Move.move(cube, cube.getSize(), 0, Axis.Z, Direction.CW);
-		System.out.println(Arrays.deepToString(cube.getCube()));
 		Move.move(cube, cube.getSize(), 0, Axis.Z, Direction.CW);
-		System.out.println(Arrays.deepToString(cube.getCube()));
 		Move.move(cube, cube.getSize(), 0, Axis.Z, Direction.CCW);
-		System.out.println(Arrays.deepToString(cube.getCube()));
 		Move.move(cube, cube.getSize(), 0, Axis.Z, Direction.CCW);
-		System.out.println(Arrays.deepToString(cube.getCube()));
-		Move.move(cube, cube.getSize(), 0, Axis.Y, Direction.CCW);
-		System.out.println(Arrays.deepToString(cube.getCube()));
+		Move.move(cube, cube.getSize(), 0, Axis.X, Direction.CCW);
 		assertTrue(cube.isSolved());
 	}
 	
